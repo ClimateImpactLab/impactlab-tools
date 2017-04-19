dbff6b8831c5eaa71c92116742f53d9cf6256a38
Michael Delgado <mdelgado@rhg.com>
2017-04-19 11:02:14 -0700

    configure pytest to run full suite by default, fix bugs in test_deployment

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,15 +1,18 @@
 
+import pytest
+
+
 def test_top_level_imports():
     import impactlab_tools
 
-    assert type(impactlab_tools) == 'module'
+    assert isinstance(impactlab_tools, type(pytest))
 
 
 def test_acp_imports():
     from impactlab_tools.acp import dist, aggregate
     from impactlab_tools.utils import cache, weighting
 
-    assert type(dist) == 'module'
-    assert type(aggregate) == 'module'
-    assert type(cache) == 'module'
-    assert type(weighting) == 'module'
+    assert isinstance(dist, type(pytest))
+    assert isinstance(aggregate, type(pytest))
+    assert isinstance(cache, type(pytest))
+    assert isinstance(weighting, type(pytest))
