5a97a8547cc057d84f60d0eac5cb654bbd8fc3da
James Rising <jarising@gmail.com>
2017-05-13 15:47:49 -0700

    ref paralog

--- tests/utils/test_paralog.py
+++ tests/utils/test_paralog.py
@@ -3,11 +3,11 @@ import shutil
 from impactlab_tools.utils import paralog
 
 def test_claiming():
-    statman1 = StatusManager('test', 'Testing process', 'testing-paralog', 60*60)
+    statman1 = paralog.StatusManager('test', 'Testing process', 'testing-paralog', 60*60)
     print statman1.logpath
     assert statman1.claim("testing-paralog"), "Cannot claim directory!"
 
-    statman2 = StatusManager('test', 'Testing process', 'testing-paralog', 60*60)
+    statman2 = paralog.StatusManager('test', 'Testing process', 'testing-paralog', 60*60)
     print statman2.logpath
     assert not statman2.claim("testing-paralog"), "Accidentally claimed directory!"
 
@@ -27,7 +27,7 @@ def test_claiming():
     print logpath1
     print logpath2
 
-    with open(StatusManager.globalstatus_filepath("testing-paralog"), 'r') as fp:
+    with open(paralog.StatusManager.globalstatus_filepath("testing-paralog"), 'r') as fp:
         print fp.read()
 
     shutil.rmtree('testing-paralog')
