c3f37cab923ec81b88c129460846974a49b7e182
Michael Delgado <delgado.michaelt@gmail.com>
2018-03-17 17:41:58 -0700

    add py3k support to setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7'
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ],
     test_suite='tests',
     setup_requires=['pytest-runner'],
