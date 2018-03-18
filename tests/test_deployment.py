b2bd6ba2bb5d7898a57ac2df7a352a0ed510a2cc
Michael Delgado <delgado.michaelt@gmail.com>
2018-03-17 17:40:01 -0700

    drop cache from tests

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -10,9 +10,8 @@ def test_top_level_imports():
 
 def test_acp_imports():
     from impactlab_tools.acp import dist, aggregate
-    from impactlab_tools.utils import cache, weighting
+    from impactlab_tools.utils import weighting
 
     assert isinstance(dist, type(pytest))
     assert isinstance(aggregate, type(pytest))
-    assert isinstance(cache, type(pytest))
     assert isinstance(weighting, type(pytest))
