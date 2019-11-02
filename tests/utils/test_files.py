05a2e3204af16643adb959aded73c7a4699bdaff
Brewster Malevich <bmalevich@rhg.com>
2019-11-01 18:00:44 -0700

    Make reload import python2 compatible for sharedpath test

--- tests/utils/test_files.py
+++ tests/utils/test_files.py
@@ -1,7 +1,12 @@
+try:
+    # For py3.
+    from importlib import reload
+except ImportError:
+    # In py27 so just use `reload()`.
+    pass
 import pytest
 from os import path as ospath
 from impactlab_tools.utils import files as ufiles
-import importlib
 
 
 @pytest.mark.parametrize('use_shellvar,expectedval',
@@ -11,7 +16,7 @@ def test_sharedpath(use_shellvar, expectedval, tmpdir, monkeypatch):
     """
     # Need to reload `ufiles` because running files.sharedpath() has
     # key sideeffect on module attributes. Need `ufiles` to be fresh for tests.
-    importlib.reload(ufiles)
+    reload(ufiles)
 
     p = tmpdir.join('foobar.yml')
     # Monkeypatch tmpdir and files default server path for test to avoid
