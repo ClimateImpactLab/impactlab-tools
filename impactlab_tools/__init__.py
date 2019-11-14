2240c5ae734496ddec81e99ebff4e04e06e8ca5c
Brewster Malevich <bmalevich@rhg.com>
2019-11-14 13:47:05 -0800

    Bump version: 0.3.1 → 0.4.0

--- impactlab_tools/__init__.py
+++ impactlab_tools/__init__.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import
 
 __author__ = """Climate Impact Lab"""
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
 _module_imports = (
 )
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extras = {
 
 setup(
     name='impactlab-tools',
-    version='0.3.1',
+    version='0.4.0',
     description="Python tools for Climate Impact Lab developers",
     long_description=readme,
     author="Climate Impact Lab",
